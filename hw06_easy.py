5bea41e855f7b4aa63fafa2faf32f6d8e0043c2e
iripo <irinysin@gmail.com>
2019-05-14 18:44:01 +0300

    Update hw06_easy.py

--- hw06_easy.py
+++ hw06_easy.py
@@ -13,16 +13,16 @@ class Triangle ():
         self.b_y = b_y
         self.c_x = c_x
         self.c_y = c_y
-        self.AB = math.sqrt(int(math.abs(((b_x - a_x)**2) + ((b_y - a_y)**2))))
-        self.AC = math.sqrt(int(math.abs(((c_x - a_x)**2) + ((c_y - a_y)**2))))
-        self.BC = math.sqrt(int(math.abs(((c_x - b_x)**2) + ((c_y - b_y)**2))))
+        self.AB = math.sqrt(int(abs(((b_x - a_x)**2) + ((b_y - a_y)**2))))
+        self.AC = math.sqrt(int(abs(((c_x - a_x)**2) + ((c_y - a_y)**2))))
+        self.BC = math.sqrt(int(abs(((c_x - b_x)**2) + ((c_y - b_y)**2))))
 
     def Pabc(self):
-        self.Pabc = round(math.sum(self.AB,self.AC,self.BC),2)
+        self.Pabc = round((self.AB+self.AC+self.BC),2)
         return (self.Pabc)
 
     def Sabc(self):
-        self.Sabc =  (round(math.abs((b_x-a_x)*(c_y-a_y)-(c_x-a_x)*(b_y-a_y))/2,2)
+        self.Sabc =  round(abs((b_x-a_x)*(c_y-a_y)-(c_x-a_x)*(b_y-a_y))/2,2)
         return (self.Sabc)
 
 
@@ -57,12 +57,12 @@ class Trapeze:
         self.c_y = c_y
         self.d_x = d_x
         self.d_y = d_y
-        self.AB = math.sqrt(int(math.abs(((b_x - a_x)**2) + ((b_y - a_y)**2))))
-        self.BC = math.sqrt(int(math.abs(((c_x - b_x)**2) + ((c_y - b_y)**2))))
-		self.CD = math.sqrt(int(math.abs(((d_x - c_x)**2) + ((d_y - c_y)**2))))
-        self.AD = math.sqrt(int(math.abs(((d_x - a_x)**2) + ((d_y - a_y)**2))))
-		self.AС = math.sqrt(int(math.abs(((с_x - a_x)**2) + ((с_y - a_y)**2))))
-		self.BD = math.sqrt(int(math.abs(((d_x - b_x)**2) + ((d_y - b_y)**2))))
+        self.AB = math.sqrt(int(abs(((b_x - a_x)**2) + ((b_y - a_y)**2))))
+        self.BC = math.sqrt(int(abs(((c_x - b_x)**2) + ((c_y - b_y)**2))))
+		self.CD = math.sqrt(int(abs(((d_x - c_x)**2) + ((d_y - c_y)**2))))
+        self.AD = math.sqrt(int(abs(((d_x - a_x)**2) + ((d_y - a_y)**2))))
+		self.AС = math.sqrt(int(abs(((с_x - a_x)**2) + ((с_y - a_y)**2))))
+		self.BD = math.sqrt(int(abs(((d_x - b_x)**2) + ((d_y - b_y)**2))))
 
     def RavineTrapeze(self):
         if self.AC == self.BD:
@@ -70,12 +70,12 @@ class Trapeze:
         return False
 
 	def Pabcd(self):
-        self.Pabcd = round(math.sum(self.AB,self.BC,self.CD,self.AD),2)
+        self.Pabcd = round((self.AB+self.BC+self.CD+self.AD),2)
         return (self.Pabcd)
  
     def Sabcd(self):
-        self.Sabc =  (round(math.abs((b_x-a_x)*(c_y-a_y)-(c_x-a_x)*(b_y-a_y))/2,2)
-		self.Sacd =  (round(math.abs((c_x-a_x)*(d_y-a_y)-(d_x-a_x)*(c_y-a_y))/2,2)
+        self.Sabc =  round(abs((b_x-a_x)*(c_y-a_y)-(c_x-a_x)*(b_y-a_y))/2,2)
+		self.Sacd =  round(abs((c_x-a_x)*(d_y-a_y)-(d_x-a_x)*(c_y-a_y))/2,2)
 		self.Sabcd = sum(self.Sabc + self.Sacd)
         return (self.Sabcd)
  
